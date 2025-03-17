86376881ff488714069e0d6f4ee6667c21d3d916
cyadlapally <cyadlapally@hndjsolutions.com>
2025-03-17 15:38:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ tweet_chain = tweet_prompt | gemini_model
 
 import streamlit as st
 
-st.header("Tweet Generator")
+st.header("H & J Solutions Tweet Generator")
 
 st.subheader("Generate tweets using Generative AI")
 
